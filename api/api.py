628fea3826d265e5d3d95f5eac80f73c6bfed616
realhardik18 <realhardik18@gmail.com>
2022-09-03 01:01:16 +0530

    got api working

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from dataExtractor import returnAllStats
+from songRecomendationSystem import returnSongIDS
 app = Flask(__name__)
 api = Api(app)
 
@@ -12,9 +13,18 @@ class PokemonDataAPI(Resource):
         parser.add_argument('id', type=int, location='args')
         data = dict(parser.parse_args())
         responseData = returnAllStats(data['id'])
-        print(responseData)
         return responseData
 
 
-api.add_resource(PokemonDataAPI, '/pokemon', endpoint='pokemon')
+class PokemonSongsRecomendationAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, location='args')
+        data = dict(parser.parse_args())
+        songs = returnSongIDS(data['id'])
+        return songs
+
+
+api.add_resource(PokemonDataAPI, '/data', endpoint='pokemon')
+api.add_resource(PokemonSongsRecomendationAPI, '/songs', endpoint='songs')
 app.run(debug=True)
