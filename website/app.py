4d80a0587a862df9747dc2ca46414bf93a198589
realhardik18 <realhardik18@gmail.com>
2022-09-03 01:43:43 +0530

    got homepage working

--- website/app.py
+++ website/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-
+import random
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/random')
+def random():
+    return render_template('data.html')
 
 # MAKE AN ALGO TO GET SONGS RELATED TO POKEMON
 # MAYBE LIKE 10
