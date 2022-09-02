ce7cd74570ea56a305c5eb43ea18c47275acc659
realhardik18 <realhardik18@gmail.com>
2022-09-02 20:51:17 +0530

    added some more methods

--- api/data-extractor.py
+++ api/data-extractor.py
@@ -1,5 +1,7 @@
+from urllib import response
 import pandas as pd
 import json
+import requests
 
 
 def getPokemonData(id):
@@ -14,10 +16,25 @@ def returnAllStats(id):
                      "Special Defense", "Speed", "accuracy", "evasion"]
     # stat id is the index of the stat +1
     data = dict()
+    data['name'] = getNameOfPokemonFromID(id)
+    data['GIF'] = getGIFOfPokemonFromID(id)
     stats = getPokemonData(id).values
     for stat in stats:
         data[PossibleStats[int(list(stat)[1]-1)]] = list(stat)[-1]
     return data
 
 
-# print(returnAllStats(1))
+def getNameOfPokemonFromID(id):
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{str(id)}')
+    return response.json()['forms'][0]['name']
+
+
+def getGIFOfPokemonFromID(id):
+    return f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/{id}.gif'
+
+
+print(returnAllStats(90))
+# print(getNameOfPokemonFromID(10))
+
+
+# print(getGIFOfPokemonFromID(2))
