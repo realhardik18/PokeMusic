1950ff61ae573eb068090edade54878a1afc83dc
realhardik18 <realhardik18@gmail.com>
2022-09-03 00:46:04 +0530

    got all the required parameters

--- api/songRecomendationSystem.py
+++ api/songRecomendationSystem.py
@@ -11,41 +11,41 @@ client = sp.client.Spotify(client_credentials_manager=creds)
 def defineLiveness(id):
     hp = int(returnAllStats(id=id)['HP'])
     if hp >= 136:
-        min_liveness, max_liveness = 0.97, 1.0
-        return [min_liveness, max_liveness]
+        target_liveness = 0.99
+        return target_liveness
     else:
-        min_liveness = hp/136
-        if min_liveness >= 0.5:
-            max_liveness = min_liveness
-            min_liveness = 0.5
-        elif min_liveness <= 0.5:
-            max_liveness = 0.5
-        return [round(min_liveness, 2), round(max_liveness, 2)]
+        target_liveness = hp/136
+        return round(target_liveness, 2)
 
 
 def defineTempo(id):
     speed = int(returnAllStats(id=id)['Speed'])
     if speed >= 132:
-        min_tempo, max_tempo = 0.97, 1.0
-        return [min_tempo, max_tempo]
+        target_tempo = 0.99
+        return target_tempo
     else:
-        min_tempo = speed/132
-        if min_tempo >= 0.5:
-            max_tempo = min_tempo
-            min_tempo = 0.5
-        elif min_tempo <= 0.5:
-            max_tempo = 0.5
-        return [round(min_tempo, 2), round(max_tempo, 2)]
+        target_tempo = speed/132
+        return round(target_tempo, 2)
+
+
+def defineEnergy(id):
+    attack = int(returnAllStats(id=id)['Attack'])
+    if attack >= 150:
+        target_energy = 0.99
+        return target_energy
+    else:
+        target_energy = attack/132
+        return round(target_energy, 2)
 
 
 def returnSongs(id):
     # reference https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recommendations
     generes = ["rock", "chill", 'classical', 'edm']
+
     recomendations = client.recommendations(seed_genres=generes, limit=10)
     return recomendations['tracks'][0]
-
 
     # work on bugs
     # possiblly enter value as a list and try]
     # work on calcultation for other variables
-print(defineTempo(6))
+print(defineEnergy(200))
