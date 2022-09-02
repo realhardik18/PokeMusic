4bd774cbf776b3855fb2c2dc92559e71f192fb45
realhardik18 <realhardik18@gmail.com>
2022-09-03 00:32:53 +0530

    got HP arrtibutes

--- api/songRecomendationSystem.py
+++ api/songRecomendationSystem.py
@@ -8,16 +8,28 @@ creds = sp.oauth2.SpotifyClientCredentials(
 client = sp.client.Spotify(client_credentials_manager=creds)
 
 
+def defineLiveness(id):
+    hp = int(returnAllStats(id=id)['HP'])
+    if hp >= 136:
+        min_liveness, max_liveness = 0.97, 1.0
+        return [min_liveness, max_liveness]
+    else:
+        min_liveness = hp/136
+        if min_liveness >= 0.5:
+            max_liveness = min_liveness
+            min_liveness = 0.5
+        elif min_liveness <= 0.5:
+            max_liveness = 0.5
+        return [round(min_liveness, 2), round(max_liveness, 2)]
+
+
 def returnSongs(id):
     # reference https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recommendations
-    artists = '0k17h0D3J5VfsdmQ1iZtE9,5WUlDfRSoLAfcVSX1WnrxN,2JY5qzEozvTdogkDTkkOMf,6eUKZXaKkcviH0Ku9w2n3V,7MSUfLeTdDEoZiJPDSBXgi'
-    generes = 'rock,chill,ambient,children,pop'
-    tracks = '6mFkJmJqdDVQ1REhVfGgd1,2J2Z1SkXYghSajLibnQHOa,4wNM9vSBzotBHn0R0fTkY5,3rmo8F54jFF8OgYsqTxm5d,1vgSaC0BPlL6LEm4Xsx59J'
-    artists, generes, tracks = artists.split(
-        ','), generes.split(','), tracks.split(',')
-    recomendations = client.recommendations(
-        seed_artists=artists, seed_genres=generes, seed_tracks=tracks, limit=10)
-    return recomendations
+    generes = ["rock", "chill", 'classical', 'edm']
+    recomendations = client.recommendations(seed_genres=generes, limit=10)
+    return recomendations['tracks'][0]
+
     # work on bugs
     # possiblly enter value as a list and try]
     # work on calcultation for other variables
+print(defineLiveness(321))
