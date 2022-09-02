9e7069f4858456e975bc9948acf4ccb269b9281b
realhardik18 <realhardik18@gmail.com>
2022-09-03 00:37:31 +0530

    got tempo aspect working

--- api/songRecomendationSystem.py
+++ api/songRecomendationSystem.py
@@ -23,13 +23,29 @@ def defineLiveness(id):
         return [round(min_liveness, 2), round(max_liveness, 2)]
 
 
+def defineTempo(id):
+    speed = int(returnAllStats(id=id)['Speed'])
+    if speed >= 132:
+        min_tempo, max_tempo = 0.97, 1.0
+        return [min_tempo, max_tempo]
+    else:
+        min_tempo = speed/132
+        if min_tempo >= 0.5:
+            max_tempo = min_tempo
+            min_tempo = 0.5
+        elif min_tempo <= 0.5:
+            max_tempo = 0.5
+        return [round(min_tempo, 2), round(max_tempo, 2)]
+
+
 def returnSongs(id):
     # reference https://developer.spotify.com/documentation/web-api/reference/#/operations/get-recommendations
     generes = ["rock", "chill", 'classical', 'edm']
     recomendations = client.recommendations(seed_genres=generes, limit=10)
     return recomendations['tracks'][0]
 
+
     # work on bugs
     # possiblly enter value as a list and try]
     # work on calcultation for other variables
-print(defineLiveness(321))
+print(defineTempo(6))
